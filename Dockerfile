0757665528816192959538a9cdce1f353e207a7e
prakharparwal <prakhar.parwal@thoughtworks.com>
2021-07-03 11:52:24 +0530

    Added docker file and post endpoint to add question

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM adoptopenjdk/openjdk11
+
+WORKDIR /usr/src/app
+
+COPY build/libs/question-service-0.0.1-SNAPSHOT.jar app.jar
+
+EXPOSE 8080
+
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/kotlin/com/question/service/handler/QuestionHandler.kt
+++ src/main/kotlin/com/question/service/handler/QuestionHandler.kt
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 
 @Controller
 class QuestionHandler {
@@ -15,4 +17,9 @@ class QuestionHandler {
         return ResponseEntity.status(HttpStatus.OK).body(temp)
     }
 
+    @PostMapping("/add/question")
+    fun addQuestion(@RequestBody question: Question){
+        println(question.id)
+        println(question.text)
+    }
 }
